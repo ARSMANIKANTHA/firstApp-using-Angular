b307f68cae37bcbde050b12e687c2b0c46c1871b
MANI <110583606+ARSMANIKANTHA@users.noreply.github.com>
2023-08-30 11:10:10 +0530

    Added Input component to HusingLocation Component

--- firstApp/src/app/housing-location/housing-location.component.ts
+++ firstApp/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   templateUrl:'./housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
